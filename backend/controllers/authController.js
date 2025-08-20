391b112cebfc044fd5d1f4988b1617973d686ddc
Dinesh Kharah <114869714+dineshkharah@users.noreply.github.com>
2025-08-20 19:29:31 +0530

    added get profile endpoint

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -74,9 +74,22 @@ const deleteUser = async (req, res) => {
     }
 }
 
+const getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.userId).select('-password');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
 
 module.exports = {
     registerUser,
     loginUser,
     deleteUser,
+    getUser,
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, loginUser, deleteUser } = require('../controllers/authController');
+const { registerUser, loginUser, deleteUser, getUser } = require('../controllers/authController');
 const authenticateUser = require('../middleware/authenticateUser');
 const { validateRegister, validateLogin } = require('../middleware/validator');
 
@@ -10,5 +10,7 @@ router.post('/register', validateRegister, registerUser);
 router.post('/login', validateLogin, loginUser);
 
 router.delete('/delete', authenticateUser, deleteUser);
+
+router.get('/get-profile', authenticateUser, getUser);
 
 module.exports = router;
